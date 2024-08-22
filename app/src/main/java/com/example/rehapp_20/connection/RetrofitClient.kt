32c3091110e33945329d88d97b87e5ef5a105fbc
JohhanPC <johanlpc05@gmail.com>
2024-08-22 15:08:58 -0500

    cambio de IP de conexion

--- app/src/main/java/com/example/rehapp_20/connection/RetrofitClient.kt
+++ app/src/main/java/com/example/rehapp_20/connection/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "http://192.168.1.64:8080"
+    private const val BASE_URL = "https://w9rrr6mq-8080.use2.devtunnels.ms/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
